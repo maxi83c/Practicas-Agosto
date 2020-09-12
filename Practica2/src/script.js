d8b2e63a5b078ab5a4c33d3b9344d9e3bf75f339
misterx83c <53312260+misterx83c@users.noreply.github.com>
2020-09-12 19:02:44 -0300

    Update script.js

--- Practica2/src/script.js
+++ Practica2/src/script.js
@@ -1,16 +1,18 @@
 const divScoreRed = document.getElementById('score-red')
 const divScoreBlue = document.getElementById('score-blue')
-const redHand = document.getElementById('mano-roja')
-const blueHand = document.getElementById('mano-azul')
-const buttonStart = document.getElementById('boton-start')
+const redHand = document.getElementById('hand-red')
+const blueHand = document.getElementById('hand-blue')
+const buttonStart = document.getElementById('buttom-start')
+const countdown = document.getElementById('timer')
 const containerGame = document.getElementById('container-game')
-const containerForm = document.getElementById('container-form')
-const containerMessage = document.getElementById('container-mensaje')
-const rock = document.getElementById('piedra')
-const paper = document.getElementById('papel')
-const scissors = document.getElementById('tijera')
-const message = document.getElementById('mensaje')
-const intentos = document.getElementById('intentos')
+const containerForm = document.getElementById('container-config')
+const containerMessage = document.getElementById('container-menssage')
+const containerRules = document.getElementById('container-rules')
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const message = document.getElementById('menssage')
+const attempts = document.getElementById('attempts')
 
 // variables para controlar puntajes y mano seleccionada
 let scoreRed = 0
@@ -19,7 +21,7 @@ let selectedHand = -1
 
 // devuelve verdadero o falso dependiendo si el juego termina
 const endGame = () => {
-    let goal = intentos.options[intentos.selectedIndex].value
+    let goal = attempts.options[attempts.selectedIndex].value
     if (goal == 0)
         return false
     goal = goal / 2
@@ -43,13 +45,15 @@ const resetGame = () => {
     redHand.classList.remove('win')
     blueHand.classList.remove('lose')
     redHand.classList.remove('lose')
-    blueHand.classList.remove('not-chose')
-    redHand.setAttribute('src','img/piedra-r.png');
-    blueHand.setAttribute('src','img/piedra-a.png');
+    blueHand.classList.remove('not__chose')
+    redHand.setAttribute('src','img/rock-red.png');
+    blueHand.setAttribute('src','img/rock-blue.png');
     buttonStart.classList.remove('invisible')
     rock.classList.add('invisible')
     paper.classList.add('invisible')
     scissors.classList.add('invisible')
+    countdown.classList.add('invisible')
+    countdown.classList.remove('countdown')
     scoreRed = 0
     scoreBlue = 0
     selectedHand = -1
@@ -71,42 +75,44 @@ const resetHands = () =>{
         redHand.classList.remove('win')
         blueHand.classList.remove('lose')
         redHand.classList.remove('lose')
-        blueHand.classList.remove('not-chose')
-        redHand.setAttribute('src','img/piedra-r.png');
-        blueHand.setAttribute('src','img/piedra-a.png');
+        blueHand.classList.remove('not__chose')
+        redHand.setAttribute('src','img/rock-red.png');
+        blueHand.setAttribute('src','img/rock-blue.png');
         buttonStart.classList.remove('invisible')
+        countdown.classList.add('invisible')
+        countdown.classList.remove('countdown')
     }         
     }, 1000)
 }
 
-// modifica las manos según las opciociones seleccionadas
+// modifica las manos según las opciones seleccionadas
 const setHands = (red, blue) =>{
-    blueHand.classList.remove('wait-blue')
-    redHand.classList.remove('wait-red')
+    blueHand.classList.remove('wait__blue')
+    redHand.classList.remove('wait__red')
     switch (red){
         case 1:
-            redHand.setAttribute('src','img/papel-r.png');
+            redHand.setAttribute('src','img/paper-red.png');
             break
         case 2:
-            redHand.setAttribute('src','img/tijera-r.png')
+            redHand.setAttribute('src','img/scissors-red.png')
     }
     switch (blue){
         case 1:
-            blueHand.setAttribute('src','img/papel-a.png');
+            blueHand.setAttribute('src','img/paper-blue.png');
             break
         case 2:
-            blueHand.setAttribute('src','img/tijera-a.png')
+            blueHand.setAttribute('src','img/scissors-blue.png')
     }
     if (blue == red){
         blueHand.classList.add('lose')
         redHand.classList.add('lose')
-    } else if ((red == 0 && blue == 1) || (red == 1 && blue == 2) || (red == 2 && blue == 0)){
-        scoreBlue++
+    } else if ((red === 0 && blue === 1) || (red === 1 && blue === 2) || (red === 2 && blue === 0)){
+        scoreBlue+=1
         divScoreBlue.innerText = scoreBlue
         blueHand.classList.add('win')
         redHand.classList.add('lose')
     } else {
-        scoreRed++
+        scoreRed+=1
         divScoreRed.innerText = scoreRed
         blueHand.classList.add('lose')
         redHand.classList.add('win')
@@ -121,16 +127,20 @@ const startGame = () => {
     rock.classList.remove('invisible')
     paper.classList.remove('invisible')
     scissors.classList.remove('invisible')
-    blueHand.classList.add('wait-blue')
-    redHand.classList.add('wait-red')
+    blueHand.classList.add('wait__blue')
+    redHand.classList.add('wait__red')
+    countdown.classList.remove('invisible')
+    countdown.classList.add('countdown')
     setTimeout(() => {
         if (selectedHand === -1){
-                scoreRed++
+                scoreRed+=1
                 divScoreRed.innerText = scoreRed
-                blueHand.classList.remove('wait-blue')
-                blueHand.classList.add('not-chose')
-                redHand.classList.remove('wait-red')
+                blueHand.classList.remove('wait__blue')
+                blueHand.classList.add('not__chose')
+                redHand.classList.remove('wait__red')
                 redHand.classList.add('win')
+                countdown.classList.add('invisible')
+                countdown.classList.remove('countdown')
                 resetHands()
         } else {
             //selecciona al azar un número entre 0 y 2
@@ -148,32 +158,38 @@ const setConfig = () => {
 containerGame.addEventListener('click', e =>{
     e.preventDefault()
     switch (e.target.className){
-        case 'boton-piedra': 
-                selectedHand = 0
-                break    
-        case 'boton-papel': 
-                selectedHand = 1
-                break
-        case 'boton-tijera': 
-                selectedHand = 2
-                break        
-        case 'boton-config': 
-                setConfig()
-                break
+        case 'buttom__rock': 
+            selectedHand = 0
+            break    
+        case 'buttom__paper': 
+            selectedHand = 1
+            break
+        case 'buttom__scissors': 
+            selectedHand = 2
+            break        
+        case 'buttom__config': 
+            setConfig()
+            break
+        case 'buttom__help':
+            containerRules.classList.remove('invisible')
+            break
         case 'start':
                 startGame()
                 break
-        case 'config-ok':
+        case 'config__ok':
             containerForm.classList.add('invisible')
             resetGame()
             break
-        case 'config-cancel':
+        case 'config__cancel':
             containerForm.classList.add('invisible')
             containerMessage.classList.add('invisible')
-            if (e.target.id === "boton-cerrar")
-                resetGame()
+            break
+        case 'rules__close':
+            containerRules.classList.add('invisible')
+            break
+        case 'popup__close':
+            containerForm.classList.add('invisible')
+            containerMessage.classList.add('invisible')
+            resetGame()
     }
 })
-
-
-
